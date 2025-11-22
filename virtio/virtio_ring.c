bed4e6f2c08ae4ebcb3b40c35a7d5615cc4df7d7
hynseok <wefwef12e@naver.com>
2025-11-22 16:39:47 +0900

    fix args

--- virtio/virtio_ring.c
+++ virtio/virtio_ring.c
@@ -636,7 +636,7 @@ static inline int virtqueue_add_split(struct virtqueue *_vq,
 				target_iova = iova_base;
 			}
 
-			addr = vring_map_one_sg(vq, sg, DMA_FROM_DEVICE, target_iova, false);
+			addr = vring_map_one_sg(vq, sg, DMA_FROM_DEVICE, 0, false);
 			if (vring_mapping_error(vq, addr))
 				goto unmap_release;
 
